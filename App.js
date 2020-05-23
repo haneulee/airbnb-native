a758afa1e7b4edafca6d48029606022a1bce371c
Haneul Lee <lovesky4294@gmail.com>
2020-05-23 17:34:30 +0900

    first commit & preload images

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import { Text, Image } from "react-native";
+
+const cacheImages = images =>
+  images.map(image => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const handleFinish = () => setIsReady(true);
+  const loadAssets = async () => {
+    const images = [
+      require("./assets/loginBg.jpeg"),
+      "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png"
+    ];
+    console.log(cacheImages(images));
+  };
+  return isReady ? (
+    <Text>I'm ready</Text>
+  ) : (
+      <AppLoading
+        onError={console.error}
+        onFinish={handleFinish}
+        startAsync={loadAssets}
+      />
+    );
+}
