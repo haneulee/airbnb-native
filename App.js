3c729dce85288399a0566ee4dd115f65c79d0108
Haneul Lee <lovesky4294@gmail.com>
2020-05-23 17:49:19 +0900

    preload Fonts

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 import { Text, Image } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+
 
 const cacheImages = images =>
   images.map(image => {
@@ -12,6 +15,8 @@ const cacheImages = images =>
     }
   });
 
+const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font))
+
 export default function App() {
   const [isReady, setIsReady] = useState(false);
   const handleFinish = () => setIsReady(true);
@@ -20,7 +25,11 @@ export default function App() {
       require("./assets/loginBg.jpeg"),
       "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png"
     ];
-    console.log(cacheImages(images));
+    const fonts = [Ionicons.font];
+    const ImagePromises = cacheImages(images);
+    const FontPromises = cacheFonts(fonts);
+
+    return Promise.all([...ImagePromises, ...FontPromises])
   };
   return isReady ? (
     <Text>I'm ready</Text>
