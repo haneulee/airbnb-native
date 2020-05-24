1d8aca3f136f1394080f100004eeb88c313bfb2e
Haneul Lee <lovesky4294@gmail.com>
2020-05-24 17:36:13 +0900

    welcome screen

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -5,6 +5,7 @@ import Welcome from "../screens/Welcome";
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
 import { Ionicons } from "@expo/vector-icons";
+import BackBtn from "../components/Auth/BackBtn";
 
 const Auth = createStackNavigator();
 
@@ -16,17 +17,18 @@ export default () => (
         screenOptions={{
             headerBackTitleVisible: false,
             headerTransparent: true,
-            headerBackImage: () => (
-                <View style={{ paddingLeft: 20 }}>
-                    <Ionicons
-                        name={isAndroid ? "md-arrow-down" : "ios-arrow-down"}
-                        size={28}
-                    />
-                </View>
-            )
+            headerBackImage: () => <BackBtn />
         }}
     >
-        <Auth.Screen name="Welcome" component={Welcome} />
+        <Auth.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{
+                headerTitleStyle: {
+                    color: "white"
+                }
+            }}
+        />
         <Auth.Screen name="SignIn" component={SignIn} />
         <Auth.Screen name="SignUp" component={SignUp} />
     </Auth.Navigator>
